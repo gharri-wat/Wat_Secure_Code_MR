bd319c6573fe16f81c67e6277342727cacbc3fcf
gharri-wat <Gharri.Wat_Course@pm.me>
2024-06-18 12:16:13 -0600

    reverted to sample.c

--- sample.c
+++ sample.c
@@ -1,18 +1,7 @@
-#include <stidio.h>
-#include <stdlib.h>
-#<include<strings.h>
-
-#define BUFSIZE 256
-int main(int argc, char** argv)
-{
-
-  if (argc !=2)
-  {
-      fprintf(stderr, "Pleaseprovide the address of a file as an input.\n");
-  }
-    return -1;
-  }
-    char cmd[BUF_SIZE] = "wc -c <");
-    strcat(cmd,argv[1];
-    system(cmd); 
-}
+#include <stdio.h>
+int main() {
+	char last_name[20];
+	printf ("Enter your last name: ");
+	scanf ("%s", last_name);
+	printf("Entered last name is %s\n", last_name);
+	}
